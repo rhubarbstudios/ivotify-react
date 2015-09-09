6844b3a7e4885269342de9de69cefff905b878ea
Alex Dinari <alexdinari@roadhouseapps.com>
2015-09-09 10:37:22 -0700

    added constant for issues, changed name of index.jsx to AppRoutes

--- src/js/constants/Constants.js
+++ src/js/constants/Constants.js
@@ -1,24 +1,37 @@
 var APIRoot = "https://ivotify-test.herokuapp.com";
 
-module.exports = {
+export default {
+  // event name triggered from store, listened to by views
+  CHANGE_EVENT: 'change',
 
   APIEndpoints: {
-    LOGIN:          APIRoot + "/api/sessions",
+    LOGIN:            APIRoot + "/api/sessions",
+    GET_ISSUES:       APIRoot + 'api/issues',
+    GET_CANDIDATES:   APIRoot + 'api/candidates'
   },
 
   PayloadSources: ({
-    SERVER_ACTION: 'SERVER_ACTION',
-    VIEW_ACTION: 'VIEW_ACTION'
+    SERVER_ACTION: "SERVER_ACTION",
+    VIEW_ACTION: "VIEW_ACTION"
   }),
 
   ActionTypes: ({
+
     // Session
-    LOGIN_REQUEST: 'LOGIN_REQUEST',
-    LOGIN_RESPONSE: 'LOGIN_RESPONSE',
+    LOGIN_REQUEST: "LOGIN_REQUEST",
+    LOGIN_RESPONSE: "LOGIN_RESPONSE",
 
     // Routes
-    REDIRECT: null
+    REDIRECT: "REDIRECT",
 
+    // Issues
+    LOAD_ISSUES: "LOAD_ISSUES",
+    RECEIVE_ISSUES: "RECEIVE_ISSUES", 
+    LOAD_ISSUE: "LOAD_ISSUE", 
+    RECEIVE_ISSUE: "RECEIVE_ISSUE",
+    CREATE_ISSUE: "CREATE_ISSUE",
+    DESTROY_ISSUE: "DESTROY_ISSUE",
+    RECEIVE_CREATED_ISSUE: "RECEIVE_CREATED_ISSUE"
       })
 
 };
